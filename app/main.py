51239e4b6c6459cef7789211263fc3b639296531
Lara Filippone <lara.filippone@gmail.com>
2025-09-11 13:32:04 +0200

    feat: add test endpoint 'chat' for frontend

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-app.include_router(router)
+app.include_router(router, prefix='/api')
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
