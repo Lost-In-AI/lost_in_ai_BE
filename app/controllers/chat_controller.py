aa09c51d71ce9c57e1f15cf81fec96dfe01967a2
Lara Filippone <lara.filippone@gmail.com>
2025-09-28 08:56:34 +0200

    feat: integrate ChatRepository and UserRepository into ChatController, refactor process_chat logic for enhanced session and message handling

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -5,67 +5,119 @@ import re
 from typing import Optional
 from uuid import uuid4
 
-from exceptions.chat_exception import BotResponseParsingError, PlaceholdersParsingError
-from schemas.enums.bot_personality import BotPersonality
-from schemas.enums.message_sender import MessageSender
-from schemas.chat_request import ChatRequest
-from schemas.chat_response import ChatResponse
-from schemas.message import Message
-from schemas.patch_chat_request import PatchChatRequest
-from schemas.patch_chat_response import PatchChatResponse
-from services.openai_service import OpenAIService
-from services import prompt_builders, prompts
 from core.configs import settings
 from utils import utc_now_isoformat
 
+from exceptions.chat_exception import BotResponseParsingError, PlaceholdersParsingError
+
+from models.message import Message
+from models.session import Session
+
+from schemas.enums.bot_personality import BotPersonality
+from schemas.enums.message_sender import MessageSender
+from schemas.message import Message as MessageSchema
+from schemas.chat_request import ChatRequest
+from schemas.chat_response import ChatResponse
+from schemas.patch_chat_request import PatchChatRequest
+from schemas.patch_chat_response import PatchChatResponse
+
+from services.clerk_service import ClerkToken
+from services.openai_service import OpenAIService
+from services import prompt_builders, prompts
+
+from repositories.user_repository import UserRepository
+from repositories.chat_repository import ChatRepository
+
 
 class ChatController:
-    def __init__(self, openai_service: OpenAIService):
+    def __init__(self, openai_service: OpenAIService, user_repository: UserRepository, chat_repository: ChatRepository):
         self.openai_service = openai_service
+        self.user_repository = user_repository
+        self.chat_repository = chat_repository
         self.MUSIC_REGEX = r"\[HOLD_MUSIC.*?\].*?\[/HOLD_MUSIC\]"
 
-    def process_chat(self, chat_request: ChatRequest) -> ChatResponse:
-        user_input_message = chat_request.current_message
-        user_message = self._to_message('user', user_input_message)
-        bot_personality = self._resolve_bot_personality(chat_request.bot_personality)
-
-        session_id = chat_request.session_id if chat_request.session_id else uuid4()
-        summary = chat_request.summary if chat_request.summary else ""
-        history = chat_request.history if chat_request.history else []
-        init_prompt = self.personality_to_prompt(bot_personality)
-        prompt = prompt_builders.prepare_prompt(init_prompt, user_input_message, history, summary)
-
-        openai_response = self.openai_service.generate_response(prompt)
-
+    def process_chat(self, chat_request: ChatRequest, token: ClerkToken = None) -> ChatResponse:
         try:
-            parsed_output = json.loads(openai_response.output[0].content[0].text)
-        except:
-            parsed_output = self.parse_or_repair_payload(openai_response.output[0].content[0].text)
+            user_input_message = chat_request.current_message
+            summary = chat_request.summary if chat_request.summary else ""
+            session_id = str(chat_request.session_id) if str(chat_request.session_id) else str(uuid4())
 
-        bot_reply = parsed_output['reply']
-        summary = parsed_output['summary']
+            bot_personality = self._resolve_bot_personality(chat_request.bot_personality)
+            init_prompt = self.personality_to_prompt(bot_personality)
 
-        bot_replies = self._handle_music_placeholders(bot_reply)
-        bot_message = self._to_message(role='assistant', message=bot_reply, personality=bot_personality)
-        bot_split_messages = [self._to_message(role='assistant', message=bot_reply, personality=bot_personality) for bot_reply in bot_replies]
+            if token:
+                user_id = token.sub
+                user = self.user_repository.get_user_by_id(user_id)
+                if not user:
+                    return ChatResponse(
+                        response_code=status.HTTP_404_NOT_FOUND,
+                        message="User not found"
+                    )
 
-        history.append(user_message)
-        history.append(bot_message)
+                if str(chat_request.session_id) and str(chat_request.session_id) != session_id:
+                    session = self.chat_repository.create_session(Session(user_id=user.id, session_id=session_id))
 
-        if openai_response.usage.input_tokens + openai_response.usage.output_tokens >= settings.MAX_TOKENS:
-            history.pop(0)
+            else:
+                if str(chat_request.session_id) and str(chat_request.session_id) != session_id:
+                    session = self.chat_repository.create_session(Session(session_id=session_id))
 
-        music = 'music' if len(bot_split_messages) > 1 else None
+            new_message = self.chat_repository.create_message(
+                Message(
+                    session_id=session_id,
+                    sender=MessageSender.USER.value,
+                    message=user_input_message,
+                    created_at=utc_now_isoformat()
+                )
+            )
 
-        return ChatResponse(
-            response_code=status.HTTP_200_OK,
-            session_id=str(session_id),
-            history=history,
-            summary=summary,
-            current_responses=bot_split_messages,
-            bot_personality=bot_personality,
-            break_reason=music if music else None
-        )
+            history = self.chat_repository.get_session_messages(session_id)
+            history_message = [self._to_message(session_id=session_id, role=message.sender, message=message.message, personality=message.bot_personality, timestamp=message.created_at) for message in history]
+
+            prompt = prompt_builders.prepare_prompt(init_prompt, user_input_message, history_message, summary)
+
+            user_message = self._to_message(session_id=session_id, role='user', message=user_input_message)
+            openai_response = self.openai_service.generate_response(prompt)
+
+            try:
+                parsed_output = json.loads(openai_response.output[0].content[0].text)
+            except:
+                parsed_output = self.parse_or_repair_payload(openai_response.output[0].content[0].text)
+
+            bot_reply = parsed_output['reply']
+            summary = parsed_output['summary']
+
+            bot_replies = self._handle_music_placeholders(bot_reply)
+            bot_message = self._to_message(session_id=session_id, role='assistant', message=bot_reply, personality=bot_personality)
+            bot_split_messages = [self._to_message(session_id=session_id, role='assistant', message=bot_reply, personality=bot_personality) for bot_reply in bot_replies]
+
+            for message in bot_split_messages:
+                message_model = Message(
+                    session_id=session_id,
+                    sender=message.sender,
+                    message=message.text,
+                    created_at=utc_now_isoformat()
+                )
+                self.chat_repository.create_message(message_model)
+
+            history_message.append(user_message)
+            history_message.append(bot_message)
+
+            if openai_response.usage.input_tokens + openai_response.usage.output_tokens >= settings.MAX_TOKENS:
+                history_message.pop(0)
+
+            music = 'music' if len(bot_split_messages) > 1 else None
+
+            return ChatResponse(
+                response_code=status.HTTP_200_OK,
+                session_id=str(session_id),
+                history=history_message,
+                summary=summary,
+                current_responses=bot_split_messages,
+                bot_personality=bot_personality,
+                break_reason=music if music else None
+            )
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
     def patch_chat(self, patch_request: PatchChatRequest) -> PatchChatResponse:
         return PatchChatResponse(
@@ -76,18 +128,19 @@ class ChatController:
 
 
     @staticmethod
-    def _to_message(role: str, message: str, personality: BotPersonality = None, timestamp: Optional[str] = None) -> Message:
+    def _to_message(session_id: str, role: str, message: str, personality: BotPersonality = None, timestamp: Optional[str] = None) -> MessageSchema:
         bot_personality = None
         if role == "user":
-            role = MessageSender.USER
+            role = 'user'
         elif role == "assistant":
-            role = MessageSender.ASSISTANT
+            role = "assistant"
             bot_personality = personality if personality else None
         else:
-            role = MessageSender.SYSTEM
+            role = "system"
 
-        return Message(
+        return MessageSchema(
             sender=role,
+            session_id=session_id,
             text=message,
             timestamp=utc_now_isoformat() if not timestamp else timestamp,
             bot_personality=bot_personality
---
+++ app/repositories/chat_repository.py
@@ -0,0 +1,36 @@
+from sqlmodel import Session as SQLSession, select
+from uuid import UUID
+
+from models.session import Session as SessionModel
+from models.message import Message
+
+
+class ChatRepository:
+    def __init__(self, db: SQLSession):
+        self.db = db
+
+
+    def create_session(self, session: SessionModel) -> SQLSession:
+        try:
+            self.db.add(session)
+            self.db.flush()
+            self.db.refresh(session)
+            return session
+        except Exception as e:
+            raise Exception(f"Error saving session with session_id {session.session_id}. Error: {e}")
+
+    def create_message(self, message: Message) -> Message:
+        try:
+            self.db.add(message)
+            self.db.flush()
+            self.db.refresh(message)
+            return message
+        except Exception as e:
+            raise Exception(f"Error saving message. Error: {e}")
+
+    def get_session_messages(self, session_id: UUID):
+        try:
+            statement = select(Message).where(Message.session_id == session_id).order_by(Message.created_at)
+            return self.db.exec(statement).all()
+        except Exception as e:
+            raise Exception(f"Error getting messages for session_id {session_id}. Error: {e}")
